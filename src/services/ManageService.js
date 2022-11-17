3e49643224f878257903d9545a0cbb24ed699954
hojin19082 <905018@naver.com>
2022-11-18 00:52:25 +0900

    test

--- src/services/ManageService.js
+++ src/services/ManageService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { renew_accessToken,initialize } from '../modules/member';
-const MANAGESURVEY_API_BASE_URL = "http://210.109.62.25:8080";
+const MANAGESURVEY_API_BASE_URL = "http://spring-boot-docker-service";
 
 const accessToken=localStorage.getItem('accessToken');
 const memberId=localStorage.getItem('memberId');
