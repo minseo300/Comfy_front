f969b31d2a219444b10ae2b80c57bcf5ed7a206d
hojin19082 <905018@naver.com>
2022-11-20 19:17:59 +0900

    test

--- src/services/CreateSurveyService.js
+++ src/services/CreateSurveyService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 
-const SURVEY_API_BASE_URL = "http://spring-boot-docker-service.test.svc.cluster.local";
+const SURVEY_API_BASE_URL = "http://spring-boot-docker-service";
 const SURVEY_SHARE_URL = "http://www.commfy.shop"
 
 class SurveyService {
